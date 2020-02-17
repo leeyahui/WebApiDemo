ebc7d80606815a1dd8dd0c86c044224560cf842f
leeyahui <liyahui900508@msn.cn>
2020-02-17 20:31:24 +0800

    1.集成了swagger 2.增加了text/plain 输入

---
+++ WebApi/Formater/PlainTextTypeFormatter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Formater
+{
+    public class PlainTextTypeFormatter : TextInputFormatter
+    {
+        public PlainTextTypeFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/plain"));
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        protected override bool CanReadType(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (encoding == null)
+            {
+                throw new ArgumentNullException(nameof(encoding));
+            }
+
+            var request = context.HttpContext.Request;
+
+            using (var reader = new StreamReader(request.Body, encoding))
+            {
+                var result = await reader.ReadToEndAsync();
+                return await InputFormatterResult.SuccessAsync(result);
+            }
+        }
+    }
+}
